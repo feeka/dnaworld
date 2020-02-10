78ef4238476fb6831e1efcbe266cdddc9a7b5980
Fee Ka <fikrattalibli@gmail.com>
2020-02-11 00:01:33 +0100

    new

--- index.py
+++ index.py
@@ -190,5 +190,6 @@ def medianstring():
         return render_template('medianstring.html',title="Median String", form=form, result=result)
     return render_template('medianstring.html',title="Median String", form=form)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+port = int(os.environ.get("PORT", 5000))
+app.run(debug=True, host='0.0.0.0', port=port)
