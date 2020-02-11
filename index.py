769039b2770740ad136ce3aa6d12dbfc8245974c
Fee Ka <fikrattalibli@gmail.com>
2020-02-11 15:56:54 +0100

    New

--- index.py
+++ index.py
@@ -7,6 +7,7 @@ from algorithms.frequent_words_with_mismatches import *
 from algorithms.minimum_skew import *
 app = Flask(__name__)
 
+
 ALGORITHMS=[
     {
         'name':'Reverse Complement',
@@ -192,4 +193,4 @@ def medianstring():
 
 import os
 port = int(os.environ.get("PORT", 5000))
-app.run(debug=True, host='0.0.0.0', port=port)
+app.run(debug=True, host='0.0.0.1', port=port)
